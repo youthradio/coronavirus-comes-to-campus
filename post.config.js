25ffbf083551f2947ac106114d5e9a7ec70c4ca5
Radames Ajna <radamajna@gmail.com>
2020-03-24 16:31:48 -0700

    fix github actions

--- post.config.js
+++ post.config.js
@@ -2,17 +2,17 @@ export default {
   baseURL: '/coronavirus-comes-to-campus',
   title: 'Coronavirus Comes to Campus',
   author: 'YR Media',
-  publishDate: 'Mar. 18, 2020',
+  publishDate: 'Mar. 24, 2020',
   location: 'Oakland, CA',
   description: 'SEO friendly Description',
-  url: 'CANONICAL URL',
+  url: 'https://interactive.yr.media/coronavirus-comes-to-campus',
   featureImage:
     'https://interactive.yr.media/college-app-guide/images/college-guide-featurex1875.jpg',
   featureImagePath: 'images/template-feature-image',
   featureImageDescription: 'Woman looking at kiosk.',
   featureImageCaption: '(Photo: Andersen Ross Photography Inc/Getty Images)',
 
-  wpPostSlug: 'north-carolina-vs-vaping-companies',
+  wpPostSlug: 'coronavirus-comes-to-campus',
   wpPostID: '60986',
   // you might not need to change
   fbAppID: '73080818131',
