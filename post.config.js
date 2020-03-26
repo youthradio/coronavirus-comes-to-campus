b30fe3aadc3c6b13ec85f8c61768b168b947b525
Radames Ajna <radamajna@gmail.com>
2020-03-25 18:30:06 -0700

    fix font name, rename feature image

--- post.config.js
+++ post.config.js
@@ -2,13 +2,13 @@ export default {
   baseURL: '/coronavirus-comes-to-campus',
   title: 'Coronavirus Comes to Campus',
   author: 'YR Media',
-  publishDate: 'Mar. 24, 2020',
+  publishDate: 'Mar. 25, 2020',
   location: 'Oakland, CA',
   description: 'It all started in early March when surreal announcements appeared, one by one, in student inboxes. Schools were closing, classes canceled or moved online, students told to clear out. We mapped our reactions to those first announcements and how we’re finding new normals during this weird and wild time to be alive.',
   url: 'https://interactive.yr.media/coronavirus-comes-to-campus',
   featureImage:
-    'https://interactive.yr.media/coronavirus-comes-to-campus/images/feature.jpg',
-  featureImagePath: 'images/feature.jpg',
+    'https://interactive.yr.media/coronavirus-comes-to-campus/images/feature-new.jpg',
+  featureImagePath: 'images/feature-new.jpg',
   featureImageDescription: 'Young Folks on Social Media talking about Coronavirus',
   featureImageCaption: '',
 
